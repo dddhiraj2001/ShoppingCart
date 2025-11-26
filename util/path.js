943001b4f41005516ec93df7e101563205c8b934
dddhiraj2001 <dddhiraj2001@gmail.com>
2025-11-25 19:37:14 -0800

    Fix path util for Vercel serverless

--- util/path.js
+++ util/path.js
@@ -1,3 +1,4 @@
 const path = require('path');
 
-module.exports = path.dirname(process.mainModule.filename);
+// Resolves to the project root (one level above /util)
+module.exports = path.join(__dirname, '..');
